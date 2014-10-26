2718bd36a633704353776b925f716e40bd736d44
doncornell <donaldccornell@gmail.com>
2014-10-26 17:55:03 -0400

    Programming Assignment 2, second version
    this one was tested with sample matrices. The 'getting cached data'
    message was uncommented.

--- cachematrix.R
+++ cachematrix.R
@@ -31,7 +31,7 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   inv <- x$getinverse()
   if (!is.null(inv)) {
-    # message('getting cached data')
+    message('getting cached data')
     return(inv)
   }
   data <- x$get()
